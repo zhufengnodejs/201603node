f0c10f1119c922fcf370aa1f13afda59f6650d93
guoyongfeng <296512521@qq.com>
2016-05-05 15:44:15 +0800

    控制器

---
+++ whole/test.js
@@ -0,0 +1,22 @@
+/*
+var obj = {name:'zfpx',age:6};
+//$scope
+// 执行上下文件环境
+with(obj){
+    console.log(name,age);
+}*/
+
+var reg = /^(?!\d+$)(?![a-zA-Z]+$)[a-zA-Z0-9]{8,15}$/;
+console.log(reg.test('1aaaaaaaaaa1'));
+
+
+// ?=  符合预查
+// ?! 不符合预查
+var reg = /^(?!\d$)/;
+console.log(reg.test('@'));
+
+
+var items = [1,2,3];
+items.forEach(function(item){
+    console.log(item);
+})
