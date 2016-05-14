26d4699e0ae6558b7cbae16b4ecdb3afb18f7fc1
guoyongfeng <296512521@qq.com>
2016-05-14 12:52:01 +0800

    观察者模式

--- 11.stream/3.writeStream.js
+++ 11.stream/3.writeStream.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var ws = fs.createWriteStream('2.txt',{
     encoding:'utf8', //设置编码是utf8
-    highWaterMark:1 //设置缓冲区大小是10个字节
+    highWaterMark:3 //设置缓冲区大小是3个字节
 });
 var count = 9;
 function _write(){
@@ -13,6 +13,7 @@ function _write(){
 }
 //当缓存区里的数据都写入文件之后会触发drain事件
 ws.on('drain',function(){
+    console.log('drain');
     _write();//继续写入
 });
 _write();
