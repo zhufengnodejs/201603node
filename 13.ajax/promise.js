d17c3bbf2af4eca28082cfe90a03dd6553c140d6
guoyongfeng <296512521@qq.com>
2016-05-15 15:22:02 +0800

    静态文件服务

--- 13.ajax/promise.js
+++ 13.ajax/promise.js
@@ -1,32 +1,40 @@
 function Promise(){
-    this._done = this.fail = this.always = function(){};
+    this._done = this._fail = this._always = function(){};
 }
 //成功回调
-Promise.done = function(done){
+Promise.prototype.done = function(done){
     this._done = done;
-}
+    return this;
+};
 //失败的回调
-Promise.fail = function(fail){
+Promise.prototype.fail = function(fail){
     this._fail = fail;
-}
+    return this;
+};
 //不管成功失败都会调用
-Promise.always = function(always){
+Promise.prototype.always = function(always){
     this._always = always;
-}
+    return this;
+};
 var fs = require('fs');
 function readFile(filename){
     var promise = new Promise();
-    fs.readFile(filename,function(err,data){
+    fs.readFile(filename,'utf8',function(err,data){
         if(err){
-
+            promise._fail(err);
+            promise._always(err);
+        }else{
+            promise._done(data);
+            promise._always(data);
         }
-    })
+    });
     return promise;
 }
+
 readFile('./1.txt').done(function(data){
-    console.log('done',data);
+    console.log('成功',data);
 }).fail(function(err){
-    console.log('fail',err);
+    console.log('失败',err);
 }).always(function(result){
     console.log('always',result);
 });
