daebb61e9db1a0fe771ff0e338292d41897df56b
guoyongfeng <296512521@qq.com>
2016-05-18 16:03:27 +0800

    页签 切换效果

--- whole/3.contact/app.js
+++ whole/3.contact/app.js
@@ -9,16 +9,30 @@ angular.module('zfpxMod').controller('contactCtrl',function($scope,$firebaseArra
         $scope.contact = {};
         $scope.addContactForm= !$scope.addContactForm
     }
+    $scope.edit = function(item){
+        $scope.contact = item;
+        $scope.addContactForm = true;
+    }
     $scope.select = function(contact){
         $scope.contact = contact;
     }
     $scope.addContact = function(){
-        contactBase.$add($scope.contact).then(function(ref){
-            var id = ref.key();
-            $scope.contact = {};
-            $scope.addContactForm = false;
-            $scope.msg = '联系人添加成功!';
-        });
+        var $id = $scope.contact.$id;
+        if($id){
+            contactBase.$save($scope.contact).then(function(ref){
+                $scope.contact = {};
+                $scope.addContactForm = false;
+                $scope.msg = '联系人修改成功!';
+            })
+        }else{
+            contactBase.$add($scope.contact).then(function(ref){
+                var id = ref.key();
+                $scope.contact = {};
+                $scope.addContactForm = false;
+                $scope.msg = '联系人添加成功!';
+            });
+        }
+
         return false;
     }
 });
