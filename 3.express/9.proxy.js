bc1c09c847b9d8e8df0acb55b96a4a0e07e8cb37
guoyongfeng <296512521@qq.com>
2016-05-25 17:51:11 +0800

    页签 切换效果

---
+++ 3.express/9.proxy.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var proxy = require('http-proxy').createProxyServer();
+var app = express();
+function proxyPass(config){
+    return function(req,res,next){
+        proxy.web(req,res,{
+            target:config[req.headers['host'].split(':')[0]]
+        });
+    }
+}
+app.use(proxyPass({
+    'zhaoxin.zfpx.cn':'http://localhost:3000',
+    'luoying.zfpx.cn':'http://localhost:4000'
+}));
+
+var app3000 = express();
+app3000.get('/',function(req,res){
+    res.send('zhaoxin');
+});
+app3000.listen(3000);
+var app4000 = express();
+app4000.get('/',function(req,res){
+    res.send('luoying');
+});
+app4000.listen(4000);
+app.listen(80);
