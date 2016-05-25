ce13b6e090c45a0752d1348bfbebb9f913a79190
guoyongfeng <296512521@qq.com>
2016-05-25 10:52:23 +0800

    页签 切换效果

--- 2.react/13.js
+++ 2.react/13.js
@@ -10,12 +10,17 @@ var Form = React.createClass({
     },
     submit:function(e){
         e.preventDefault();
+        var form = {};
         var inputValue =   this.refs.inputText.value;
         var selectValue =   this.refs.selectText.value;
         var areaValue =   this.refs.areaText.value;
-        this.setState({inputValue:inputValue});
-        this.setState({selectValue:selectValue});
-        this.setState({textAreaValue:areaValue});
+        //setState是异步的，如果要写后续逻辑必须写在回调函数中
+        this.setState({inputValue:inputValue,
+            selectValue:selectValue,
+            textAreaValue:areaValue
+        },function(){
+            console.log(this.state);
+        }.bind(this));
         console.log(this.state);
         return false;
     },
@@ -23,14 +28,26 @@ var Form = React.createClass({
         this.setState({radioValue:e.target.value});
     },
     handleCheck:function(e){
-
+        //先取出原来的复选框数组
+        var checkValues = this.state.checkValue;
+        //得到当前选中的复选框的值
+        var newVal = e.target.value;
+        //获取复选框的值在原来数组中的索引
+        var index = checkValues.indexOf(newVal);
+        //如果索引等于-1的话 意味着原来没选 中，现在要选 中，就是把这个值添加到原来的数组中
+        if(index==-1){
+            checkValues.push(newVal);
+        }else{
+            //否则就是要移除，即从原来数组中删除掉
+            checkValues.splice(index,1);
+        }
     },
     render:function(){
         return (
             <form onSubmit={this.submit} >
                 <input ref="inputText" className="form-control" type="text" defaultValue={this.state.inputValue}/>
                 <br/>
-                <select onChange={this.handleSelect} ref="selectText" className="form-control"  defaultValue={this.state.selectValue} >
+                <select ref="selectText" className="form-control"  defaultValue={this.state.selectValue} >
                     <option value="A">A</option>
                     <option value="B">B</option>
                     <option value="C">C</option>
