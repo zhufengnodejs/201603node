6c2205b22f333f0627a8d292c9cc10bf306d715a
guoyongfeng <296512521@qq.com>
2016-05-28 10:53:35 +0800

    express中的路由

--- 17.express/2.route.js
+++ 17.express/2.route.js
@@ -2,13 +2,16 @@ var express = require('express');
 // express是一个函数，调用可以得到app应用
 var app = express();
 var fs = require('fs');
+//开闭原则 对扩展开放，对修改关闭  能添加新功能，不能修改老代码
 fs.readdir('./routers',function(err,files){
-    if(err)
-    console.error(err);
+    if(err){
+        return console.error(err);
+    }else{
+        files.forEach(function(file){
+            console.log(file);
+            require('./routers/'+file)(app);
+        })
+    }
 
-    files.forEach(function(file){
-        console.log(file);
-        require('./routers/'+file)(app);
-    })
 })
 app.listen(9090);
