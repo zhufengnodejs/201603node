2a40cd6aeaa353f592529f4526fec0d01806dbb7
guoyongfeng <296512521@qq.com>
2016-05-28 16:34:50 +0800

    可以有空格

--- 17.express/9.static.js
+++ 17.express/9.static.js
@@ -6,7 +6,7 @@ var app = express();
 //静态文件中间件
 //static 会返回一个中间件函数
 //接收一个静态文件存放的绝对目录作为参数
-app.use(static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/hello',function(req,res){
     res.end('hello');
 });
